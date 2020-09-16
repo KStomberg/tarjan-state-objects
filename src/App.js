718b39be115aa9577d1fd8a1003afe7f3c9b5d74
SinnyDE <57651545+SinnyDE@users.noreply.github.com>
2020-09-16 14:35:58 -0500

    basic app component

--- src/App.js
+++ src/App.js
@@ -2,25 +2,21 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  //Define our initial state
+  state= {
+    cohortName: 'Tarjan',
+    salutation: 'Hello!',
+    name: 'Kieran'
+  }
+  render() {
+    return (
+      <div>
+        <h1>{this.state.cohortName} learns state with objects</h1>
+    <div>{this.state.salutation} my name is {this.state.name}</div>
+      </div>
+    )
+  }
 }
 
 export default App;
